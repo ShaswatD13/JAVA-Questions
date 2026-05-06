5cf1fb64813724ed20a2bddbfb73c7d58cb449c2
shaswatdwivedi <shasvedi13@gmail.com>
2026-05-06 20:13:45 +0530

    Printing the details of the student object

---
+++ STUDENT.java
@@ -0,0 +1,23 @@
+class Student
+{
+  String name;
+  int Roll_No;
+  float CGPA;
+}
+
+public String toString()
+{
+  return name = shaswat\n
+}
+
+class Launch
+{
+  public static void main(String[] args)
+  {
+   Student ref = new Student();
+   System.out.println(ref.name);
+   System.out.println(ref.Roll_No);
+   System.out.println(ref.CGPA);
+
+  }
+}
