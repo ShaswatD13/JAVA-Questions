ef446929f6d6f39a2a74e2baf56f6fb6e1dfbfaa
ShaswatD13 <shasvedi13@gmail.com>
2026-05-28 21:23:44 +0530

    QUIZ

---
+++ PredictQ64.c
@@ -0,0 +1,19 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 3;
+    int c = a & b;
+    int d = a | b;
+
+    printf("%d %d", c, d);
+
+    return 0;
+}
+
+
+// 1 7
