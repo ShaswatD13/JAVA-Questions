09885ccbf29f35f80fb4b211bf6dab7462bca97a
ShaswatD13 <shasvedi13@gmail.com>
2026-05-31 22:27:27 +0530

    Question

---
+++ PredictQ40.c
@@ -0,0 +1,21 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+
+    if (x = 5)
+        printf("Hello ");
+    else
+        printf("Hi ");
+
+    printf("%d", x);
+
+    return 0;
+}
+
+
+// Hello 5
