f5bba46e28328429dd8623c5f93b97d8a8e7fc54
ShaswatD13 <shasvedi13@gmail.com>
2026-09-04 21:36:56 +0530

    Predict

---
+++ PredictQ102.c
@@ -0,0 +1,17 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for(i = 0; i < 3; printf("%d ", i), i++)
+        ;
+
+    return 0;
+}
+
+// 0 1 2
